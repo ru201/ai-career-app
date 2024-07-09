44c73d1994e618ea06c9af8cb90131ecb663a910
ru201 <rubinroy12@gmail.com>
2024-07-09 14:51:16 +1000

    Finish data collection activities

--- career-app/src/pages/chatbot.js
+++ career-app/src/pages/chatbot.js
@@ -8,7 +8,7 @@ import { MainContainer, ChatContainer, MessageInput, TypingIndicator, MessageLis
 
 export default function Chatbot () {
 
-    const API_KEY = process.env.REACT_APP_API_KEY;
+    const AI_KY = process.env.REACT_APP_API_KEY;
 
     const [typing, setTyping] = useState(false)
     const [messages, setMessages] = useState([
@@ -67,7 +67,7 @@ export default function Chatbot () {
           await fetch("https://api.openai.com/v1/chat/completions", {
             method: "POST",
             headers: {
-              "Authorization": "Bearer " + API_KEY,
+              "Authorization": "Bearer " + AI_KY,
               "Content-Type": "application/json"
             },
             body: JSON.stringify(apiRequestBody)
